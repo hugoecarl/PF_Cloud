5b6a4f7852df7c1dc5bc1e090c0fc6e21c2df757
hugoecarl <mormeme@gmail.com>
2019-11-17 20:11:37 -0300

    flask_app

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, jsonify, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+
+app = Flask(__name__, static_url_path="")
+api = Api(app)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Hugo Carl',
+        'description': u'Programa Python',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Rafael R',
+        'description': u'Banco de dados SQL',
+        'done': False
+    }
+]
+
+task_fields = {
+    'id' : fields.Integer,
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean
+    #'uri': fields.Url('task')
+}
+
+
+class HealthCheck(Resource):
+    def get(self):
+        return 200
+
+
+
+
+class TaskListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str, default="",
+                                   location='json')
+        super(TaskListAPI, self).__init__()
+
+    def get(self):
+        return {'tasks': [marshal(task, task_fields) for task in tasks]}
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        task = {
+            'id': tasks[-1]['id'] + 1 if len(tasks) > 0 else 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+        return {'task': marshal(task, task_fields)}, 201
+
+
+class TaskAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        return {'task': marshal(task[0], task_fields)}
+
+    def put(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                task[k] = v
+        return {'task': marshal(task, task_fields)}
+
+    def delete(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        tasks.remove(task[0])
+        return {'result': True}
+
+
+api.add_resource(TaskListAPI, '/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/tasks/<int:id>', endpoint='task')
+api.add_resource(HealthCheck, '/healthcheck/', endpoint='healthcheck')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
