1266cb15fe50f0782b907cebf9056cef6083fd51
hugoecarl <mormeme@gmail.com>
2019-11-19 18:57:00 -0300

    cria base

---
+++ MongoDBinit.py
@@ -0,0 +1,24 @@
+import pymongo
+
+connection = pymongo.MongoClient('localhost', 27017)
+
+database = connection['mydb_1']
+
+collection = database['mycol_1']
+
+data = {
+        'title': u'Hugo Carl',
+        'description': u'Programa Python',
+        'done': False
+    }
+data1 = {
+        'title': u'Gabriel M',
+        'description': u'Banco de dados SQL',
+        'done': False
+    }
+
+collection.insert_one(data)
+collection.insert_one(data1)
+
+for i in collection.find({}):
+    print("dados inseridos - " +i)
