ff161b6dc8f16113b6ce6a556e7b95a31e8a1a3d
Ubuntu <ubuntu@ip-172-31-26-39.ec2.internal>
2019-11-19 22:17:47 +0000

    mongosh

---
+++ install_mongo.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install python3-pip -y
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+pip3 install pymongo
+
