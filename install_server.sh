dacd8a8e2967d32b6e1d55c177f26ca584de5d5b
Hugo Carl <30904672+hugoecarl@users.noreply.github.com>
2019-11-19 01:18:36 -0300

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt install python3-pip -y
+sudo apt install python3-flask -y
 pip3 install flask
 pip3 install flask_restful
 sudo ufw allow 5000
 cd /home/ubuntu/PF_Cloud
-python3 app.py
+export FLASK_APP=app.py
+flask run --host=0.0.0.0
