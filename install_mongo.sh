a178a2c14c8ef48877aa17763ee055f1aa3554cb
Ubuntu <ubuntu@ip-172-31-87-120.ec2.internal>
2019-11-20 02:12:59 +0000

    instmong

--- install_mongo.sh
+++ install_mongo.sh
@@ -4,6 +4,7 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo service mongod start
 pip3 install pymongo
+sudo mkdir -p /data/db
+sudo mongod --bind_ip 0.0.0.0 -v
 
