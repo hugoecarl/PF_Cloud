640c6297611e9878f69697b0c788a551d0095726
hugoecarl <mormeme@gmail.com>
2019-11-21 19:46:44 -0300

    pass_on

---
+++ pass_on.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, abort, make_response
+from flask_restful import Api, Resource, reqparse, fields, marshal
+import requests
+
+connection = "http://3.94.152.209:5000/"
+
+app = Flask(__name__, static_url_path="")
+api = Api(app)
+
+task_fields = {
+    'id' : fields.Integer,
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean
+}
+
+class HealthCheck(Resource):
+    def get(self):
+        return 200
+
+class TaskListAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str, default="",
+                                   location='json')
+        super(TaskListAPI, self).__init__()
+
+    def get(self):
+        return {'tasks': [marshal(task, task_fields) for task in requests.get(connection+"tasks/")]}
+
+    # def post(self):
+    #     args = self.reqparse.parse_args()
+    #     task = {
+    #         'id': tasks[-1]['id'] + 1 if len(tasks) > 0 else 1,
+    #         'title': args['title'],
+    #         'description': args['description'],
+    #         'done': False
+    #     }
+    #     tasks.append(task)
+    #     return {'task': marshal(task, task_fields)}, 201
+
+api.add_resource(TaskListAPI, '/tasks', endpoint='tasks')
+api.add_resource(HealthCheck, '/healthcheck/', endpoint='healthcheck')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
+
+
