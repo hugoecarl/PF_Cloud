08bb6cd6015cae54d60f4347c88764f32bc34adf
Hugo Carl <30904672+hugoecarl@users.noreply.github.com>
2019-11-21 20:53:09 -0300

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -4,6 +4,7 @@ sudo apt install python3-pip -y
 sudo apt install python3-flask -y
 pip3 install flask
 pip3 install flask_restful
+pip3 install pymongo
 sudo ufw allow 5000
 cd /home/ubuntu/PF_Cloud
 python3 MongoDBinit.py
