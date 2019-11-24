54a0b55d2b8dd2efd18c95135e574a64a509a9c3
Hugo Carl <30904672+hugoecarl@users.noreply.github.com>
2019-11-24 18:24:25 -0300

    Update install_passon.sh

--- install_passon.sh
+++ install_passon.sh
@@ -5,7 +5,7 @@ sudo apt install python3-flask -y
 pip3 install flask
 pip3 install flask_restful
 sudo ufw allow 5000
-crontab -l | { cat; echo "@reboot cd /home/ubuntu/PF_Cloud;export FLASK_APP=pass_on.py;flask run --host=0.0.0.0"; } | crontab -
+crontab -l | { cat; echo "@reboot export MYVAR=`cat /home/ubuntu/ippasson.txt`;cd /home/ubuntu/PF_Cloud;export FLASK_APP=pass_on.py;flask run --host=0.0.0.0"; } | crontab -
 cd /home/ubuntu/PF_Cloud
 export FLASK_APP=pass_on.py
 flask run --host=0.0.0.0
